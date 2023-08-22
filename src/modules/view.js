cabb852d1fc9d470eca7d238a24620afb439de1e
Dmitry <krdvmail@mail.ru>
2023-08-22 12:33:41 +0300

    refactor 3_3

--- src/modules/view.js
+++ src/modules/view.js
@@ -106,7 +106,8 @@ const handleValidUrl = (elements, i18n, value) => {
   showFeed(elements, feedTitle, feedDescription);
   initPosts(elements);
   addPosts(elements, i18n, feedPosts, feedDescription);
-  setTimeout(() => elements.form.reset(), 100);
+  // setTimeout(() => elements.form.reset(), 100);
+  elements.form.reset();
 };
 
 const render = (elements, i18n) => (path, value) => {
